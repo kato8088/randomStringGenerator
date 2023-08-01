9a2aaf611e65b9061ab38533b8fa4c1888646279
Kato <gabriel.kato22@uol.com.br>
2023-08-01 16:25:13 -0300

    release da versao 1.1 do randomStringGenerator!

--- mainWin64.cpp
+++ mainWin64.cpp
@@ -5,47 +5,81 @@
 
 using namespace std;
 
-static const char AlphaNumeric[] = "0123456789"
+static const char AlphaNumericSimple[] = "0123456789"
 "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 "abcdefghijklmnopqrstuvwxyz";
-int Lenght = sizeof(AlphaNumeric) - 1;
+int LenghtSimple = sizeof(AlphaNumericSimple) - 1;
 
-char RandomStr() {
-    return AlphaNumeric[rand() % Lenght];
+static const char AlphaNumericComplex[] = "0123456789"
+"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+"abcdefghijklmnopqrstuvwxyz"
+"!#$";
+int LenghtComplex = sizeof(AlphaNumericComplex) - 1;
+
+char RandomSimple() {
+    return AlphaNumericSimple[rand() % LenghtSimple];
+}
+
+char RandomComplex() {
+    return AlphaNumericComplex[rand() % LenghtComplex];
 }
 
 int main() {
-    bool genAgain = true;
-    while (genAgain = true) {
-        cout << "Amount of strings to be generated: ";
-        int strRemaining;
-        cin >> strRemaining;
-        cout << "Length of the generated strings: ";
-        int strLenght;
-        cin >> strLenght;
+start:
+    char simpleComplexStr;
+    cout << "Generate simple or complex strings? (s/c): ";
+    cin >> simpleComplexStr;
+    cout << "Amount of strings to be generated: ";
+    int strRemaining;
+    cin >> strRemaining;
+    cout << "Lenght of the generated strings: ";
+    int strLenght;
+    cin >> strLenght;
+    srand(time(0));
+    switch (simpleComplexStr)
+    {
+    case 's':
         system("cls");
-        srand(time(0));
-        while (strRemaining > 0) {
-            // srand(time(0));
+        while (strRemaining > 0)
+        {
             for (int m = 0; m < strLenght; m++) {
-                cout << RandomStr();
+                cout << RandomSimple();
             }
             strRemaining--;
             cout << endl << endl;
-        }
-        char yesNo;
-        cout << "Generate more strings? (y/n): ";
-        cin >> yesNo;
-              
-        switch (yesNo) {
-        case 'Y': case 'y':
-            genAgain = true;
-            system("cls");
-            break;
-        case 'N': case 'n':
-            genAgain = false;
-            return 1;
-        }
+        };
+        break;
+    case 'c':
+        system("cls");
+        while (strRemaining > 0)
+        {
+            for (int m = 0; m < strLenght; m++) {
+                cout << RandomComplex();
+            }
+            strRemaining--;
+            cout << endl << endl;
+        };
+        break;
+    default:
+        system("cls");
+        cin.clear();
+        cout << "Wrong input! Try again! \n";
+        goto start;
+        break;
+    }
+    cout << "Generate more strings? (y/n): ";
+    char yesNo;
+    cin >> yesNo;
+    bool genAgain;
+    switch (yesNo)
+    {
+    case 'y': case 'Y':
+        genAgain = true;
+        system("cls");
+        goto start;
+        break;
+    default:
+        return 1;
     }
     return 0;
 }
